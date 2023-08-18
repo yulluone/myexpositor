de2f91fb541f3e48dc4158b93610f4bf09f50ea6
yulluone <joshuamuuo5@gmail.com>
2023-08-18 18:40:59 +0300

    fixed the scrolling issue for right and left section added responsiveness sidebar is hidden in small screens added a top nav that is hidden in larger screen TODO: move the top bar to a different component added button for opening and closing the menu TODO: wrap the icon(img) in button tags

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,69 +5,50 @@ import BibleIcon from "./icons/BibleIcon";
 import Link from "next/link";
 
 import TableOfContents from "./tableOfContents";
+import Logo from "./Logo";
+import TOC from "./toc";
 
 const Nav = ({ children }) => {
-  const [menuIsOpen, setMenuIsOpen] = useState(true);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
 
   const handleToggleMenu = () => {
+    document.getElementById("menu").style.display = menuIsOpen
+      ? "none"
+      : "inline";
+
     setMenuIsOpen(!menuIsOpen);
   };
 
   return (
-    <div className="h-screen w-full bg-white md:grid-cols-3 grid grid-cols-1">
+    <div className="h-screen w-full  ">
       {/* <!-- Nav --> */}
-      <aside className="md:h-full flex flex-wrap md:block md:flex-row md:w-full w-full h-16 mx-auto items-center bg-gray-800 text-white ">
-        {/* <!-- Logo --> */}
-        <Link
-          href="/"
-          className="h-10 mr-auto text-white w-32 px-2 flex justify-center items-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white"
+      <section className="sm:hidden w-full h-16 bg-gray-800 fixed pointer mb-2">
+        <div
+          className="w-10 h-10 fixed right-6 top-4 "
+          onClick={handleToggleMenu}
         >
-          My Expositor
-        </Link>
-
-        {/* <!-- menu --> */}
-        <div className="block ml-auto text-base  items-center ">
-          {/* <!-- icon --> */}
-          <div className="block md:hidden">
-            <button
-              onClick={handleToggleMenu}
-              className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
-            >
-              <svg
-                className={`fill-current h-3 w-3 ${
-                  menuIsOpen ? "hidden" : "block"
-                }`}
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-              <svg
-                className={`fill-current h-3 w-3 ${
-                  menuIsOpen ? "block" : "hidden"
-                }`}
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
-              </svg>
-            </button>
-          </div>
-
-          {/* <!-- dropdown --> */}
-          <div
-            id="dropdownNavbar"
-            className={`z-50  divide-y ${
-              menuIsOpen ? "" : "hidden"
-            } divide-gray-100 rounded-lg bg-gray-800 shadow `}
-          >
-            <TableOfContents />
-          </div>
+          <img
+            src={menuIsOpen ? "/icons/menu-close.png" : "/icons/menu-open.png"}
+            alt=""
+          />
         </div>
-      </aside>
-      <aside className="relative overflow-y-scroll col-span-2 px-10">
-        {children}
-      </aside>
+      </section>
+      <div className="flex">
+        <section
+          id="menu"
+          style={{ "min-width": "240px" }}
+          className="hidden sm:w-1/3 md:w-1/2 sm:relative mt-16 sm:mt-0 h-full sm:inline fixed w-full items-center overflow-y-scroll bg-gray-800 p-3"
+        >
+          <div className="hidden sm:inline">
+            <Logo />
+          </div>
+          {/* <TableOfContents /> */}
+          <TOC />
+        </section>
+        <section className="sm:fixed right-0 sm:w-2/3 md:w-1/2 overflow-y-scroll col-span-2 px-10">
+          {children}
+        </section>
+      </div>
     </div>
   );
 };
