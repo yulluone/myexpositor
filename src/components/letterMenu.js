9848e43c5b72e7007edbd3c4f237708a64e35f21
yulluone <joshuamuuo5@gmail.com>
2023-08-21 19:03:37 +0300

    added missing C to the letter menu

--- src/components/letterMenu.js
+++ src/components/letterMenu.js
@@ -1,15 +1,19 @@
 import { useState } from "react";
-import { TableOfContentsData } from "./tableOfContentsData";
-import letterMenuItem from "./letterMenuItem";
+import { BooksOfTheBible } from "./tableOfContentsData";
 import LetterMenuItem from "./letterMenuItem";
 
-export default function LetterMenu({ handleToggleMenu }) {
+export default function LetterMenu({
+  handleToggleMenu,
+  selectedBook,
+  setSelectedBook,
+}) {
   //selected letter state
   const [selectedLetter, setSelectedLetter] = useState(" ");
   const [showBooks, setShowBooks] = useState([]);
   //starting letters for the books of the bible
   const letters = [
     "A",
+    "C",
     "D",
     "E",
     "G",
@@ -28,22 +32,24 @@ export default function LetterMenu({ handleToggleMenu }) {
   ];
 
   const handleLetterSelect = ({ letter }) => {
-    setShowBooks(TableOfContentsData.filter((book) => book.name[0] === letter));
+    setShowBooks(BooksOfTheBible.filter((book) => book.name[0] === letter));
     letter === selectedLetter
       ? setSelectedLetter("")
       : setSelectedLetter(letter);
   };
 
   return (
-    <div className="w-full h-full block px-10 overflow-y-scroll ">
+    <div className="w-full h-full block px-10 mt-32 pb-32 overflow-y-scroll ">
       {letters.map((letter) => (
-							<LetterMenuItem
-										key={letter}
+        <LetterMenuItem
+          key={letter}
           letter={letter}
           handleLetterSelect={handleLetterSelect}
           handleToggleMenu={handleToggleMenu}
           selectedLetter={selectedLetter}
           showBooks={showBooks}
+          selectedBook={selectedBook}
+          setSelectedBook={setSelectedBook}
         />
       ))}
     </div>
