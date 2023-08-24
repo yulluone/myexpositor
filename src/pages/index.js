e3a017ac42ff0afebaddd43e8bd1ca167359c4b2
yulluone <joshuamuuo5@gmail.com>
2023-08-24 09:21:04 +0300

    increases mt for home page  and added a pb

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen mt-16">
-      <h1 className="text-3xl text-center font-bold mb-4">
+    <div className="flex flex-col items-center justify-center h-screen mt-32 pb-32">
+      <h1 className=" text-lg sm:text-3xl text-center font-bold mb-4">
         Welcome to the Expositor's Bible
       </h1>
-      <p className="text-lg text-center mb-6">
+      <p className="text-sm sm:text-lg text-center mb-6">
         Ranked the best Bible Study Version, The Expositor's Bible(KJV) is a
         unique collection of insights, commentary, and wisdom from various
         scholars and theologians. It provides a deeper understanding of the Holy
@@ -14,10 +14,10 @@ export default function Home() {
         historical context.
       </p>
 
-      <p className="text-lg text-center mb-6">
+      <p className="text-sm sm:text-lg text-center mb-6">
         May your journey through the Word be enlightening and transformative.
       </p>
-      <p className="text-lg text-center text-green-700">
+      <p className="text-sm sm:text-lg text-center text-green-700">
         Use the Menu to start exploring or jump right in from{" "}
         <span>
           <Link href="/bible/genesis-01">The Beginning</Link>
